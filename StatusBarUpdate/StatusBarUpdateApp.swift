892fccdff4a6f007b856aaaf7876923e58beb929
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2023-10-04 14:04:52 -0500

    Initial Commit

---
+++ StatusBarUpdate/StatusBarUpdateApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatusBarUpdateApp.swift
+//  StatusBarUpdate
+//
+//  Created by Justin Hold on 10/4/23.
+//
+
+import SwiftUI
+
+@main
+struct StatusBarUpdateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

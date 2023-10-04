caf278472352735067d71483c1dc06caa1ee0766
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2023-10-04 14:37:09 -0500

    first commit

---
+++ StatusBarUpdate/StatusBarView.swift
@@ -0,0 +1,18 @@
+//
+//  StatusBarView.swift
+//  StatusBarUpdate
+//
+//  Created by Justin Hold on 10/4/23.
+//
+
+import SwiftUI
+
+struct StatusBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StatusBarView()
+}
